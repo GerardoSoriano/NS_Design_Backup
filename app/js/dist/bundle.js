b991e7816c233f69321a12e1ad499100c21784d7
Gerardo Soriano <gerardosoriano97@gmail.com>
2017-04-21 18:01:02 -0500

    Se terminan las categorias

--- app/js/dist/bundle.js
+++ app/js/dist/bundle.js
@@ -76,6 +76,38 @@ $(document).ready(function(){
     $('div[class^="header"]').toggleClass('header header-max');
     $('div[class^="content"]').toggleClass('content content-max');
   });
+  $('div.categoria>div.offers').slick({
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true
+  });
+  $('div.categoria>div.section>div.products').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+    {
+      breakpoint: 1572,
+      settings: {
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 1254,
+      settings: {
+        slidesToShow: 2
+      }
+    },
+    {
+      breakpoint: 936,
+      settings: {
+        slidesToShow: 1
+      }
+    }
+  ]
+  });
 });
 
 
