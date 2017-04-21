02d57ad61aa9b2009fd35faafe322f31dc8b1913
Gerardo Soriano <gerardosoriano97@gmail.com>
2017-04-20 20:31:05 -0500

    Se termina el dashboard, falta hacerlo responsivo

--- app/js/app/index.js
+++ app/js/app/index.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
-  $('div[class*="content"]>div.header>div.button').on("click",function(){
-    $('div[class^="menu"]').toggleClass("menu menu-min");
-    $('div[class*="content"]').toggleClass("content content-max");
+  $('div[class^="header"]>div.button').on("click",function(){
+    $('div[class^="menu"]').toggleClass('menu menu-min');
+    $('div[class^="header"]').toggleClass('header header-max');
+    $('div[class^="content"]').toggleClass('content content-max');
   });
 });
